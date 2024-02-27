75b6fa891b37fefc452f88005deb3862d5b15954
seongu <tjsrn183@naver.com>
2024-02-27 17:39:43 +0900

    Fix: 데스크탑환경에서 x버튼 클릭먹통 해결

--- app/join/@location/(.)locationModal/page.tsx
+++ app/join/@location/(.)locationModal/page.tsx
@@ -11,19 +11,19 @@ export default function Location() {
   };
 
   return (
-    <>
-      <div className="fixed inset-0 z-40 bg-gray-700 opacity-50"></div>
-      <div className="fixed inset-0 z-50 flex items-center justify-center">
+    <div>
+      <div className="fixed inset-0 z-30 bg-gray-700 opacity-50"></div>
+      <div className="fixed inset-0 z-40 flex items-center justify-center">
         <div className="w-4/5 h-2/5 text-center bg-white rounded-[6px] shadow-2xl flex flex-col p-2">
-          <div className="flex justify-end">
+          <div className="relative z-50 flex justify-end">
             <button className="mt-2 mr-2" onClick={onClickClose}>
-              <Image src={close} alt="close" />
+              <Image src={close} alt="close" width={10} height={10} />
             </button>
           </div>
 
           <Map />
         </div>
       </div>
-    </>
+    </div>
   );
 }
