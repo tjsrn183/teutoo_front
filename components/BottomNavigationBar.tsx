9278f2a9b3e9c1aa575dd9f87351da7d4286c0cf
전동헌 <56753677+qaws7791@users.noreply.github.com>
2024-02-28 18:19:37 +0900

    Fix: BottomNavigation 컴포넌트 개선, cn 함수 추가

--- components/BottomNavigationBar.tsx
+++ components/BottomNavigationBar.tsx
@@ -1,23 +1,57 @@
-import Image from "next/image";
-import home from "../public/bottomNavIcons/home.png";
-import calc from "../public/bottomNavIcons/calc.png";
-import me from "../public/bottomNavIcons/me.png";
-import chat from "../public/bottomNavIcons/chat.png";
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils/tailwind.utils";
+import {
+  Home,
+  ClipboardList,
+  MessageSquareMore,
+  UserRound,
+} from "lucide-react";
+const NAVIGATION_ITEMS = [
+  {
+    id: "home",
+    path: "/",
+    name: "홈",
+    icon: Home,
+  },
+  {
+    id: "calc",
+    path: "/calc",
+    name: "견적",
+    icon: ClipboardList,
+  },
+  {
+    id: "chat",
+    path: "/chat",
+    name: "채팅",
+    icon: MessageSquareMore,
+  },
+  {
+    id: "account",
+    path: "/account",
+    name: "나",
+    icon: UserRound,
+  },
+];
+
 export default function BottomNavigationBar() {
+  const path = usePathname();
   return (
-    <div className=" fixed bottom-0 bg-white p-2 flex justify-around z-30 md:left-1/2 md:transform md:-translate-x-1/2 md:w-full w-full ">
-      <button>
-        <Image src={home} alt="home" />
-      </button>
-      <button>
-        <Image src={calc} alt="calc" />
-      </button>
-      <button>
-        <Image src={chat} alt="chat" />
-      </button>
-      <button>
-        <Image src={me} alt="me" />
-      </button>
+    <div className=" fixed bottom-0 bg-white h-14 flex z-30 md:left-1/2 md:transform md:-translate-x-1/2 w-full">
+      {NAVIGATION_ITEMS.map((item) => (
+        <Link
+          href={item.path}
+          key={item.id}
+          className={cn(
+            "w-full flex flex-col justify-center items-center hover:bg-neutral-100",
+            path === item.path ? "text-green-600" : " text-neutral-500",
+          )}
+        >
+          <item.icon />
+          <span className="text-sm">{item.name}</span>
+        </Link>
+      ))}
     </div>
   );
 }
