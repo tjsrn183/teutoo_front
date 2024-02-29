45559b7defa898a40d59779740d6a65ef036e2c0
seongu <tjsrn183@naver.com>
2024-02-29 14:06:36 +0900

    Fix: 지도 에러시 페이지 전체 먹통방지를 위해 옵셔널체이닝적용

---
+++ app/schedule/layout.tsx
@@ -0,0 +1,11 @@
+import { SmallHeader } from "@/components/SmallHeader";
+import { ReactNode } from "react";
+
+export default function ScheduleLayout({ children }: { children: ReactNode }) {
+  return (
+    <>
+      <SmallHeader title="PT 일정" />
+      {children}
+    </>
+  );
+}
