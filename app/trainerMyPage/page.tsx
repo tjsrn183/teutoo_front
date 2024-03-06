d6b4d7cf97cb0ca48d30564a99fa9bfde9257fa0
seongu <tjsrn183@naver.com>
2024-03-06 16:39:18 +0900

    Refactor: 추상화 강화

--- app/trainerMyPage/page.tsx
+++ app/trainerMyPage/page.tsx
@@ -1,45 +1,33 @@
-import Image from "next/image";
-import userThumb from "@/public/trainerMyPageIcons/userThumb.png";
-import logout from "@/public/trainerMyPageIcons/logoutButton.png";
-import infoEditButton from "@/public/trainerMyPageIcons/infoEditButton.png";
 import schedule from "@/public/trainerMyPageIcons/schedule.png";
 import programManage from "@/public/trainerMyPageIcons/programManage.png";
 import trainerInfo from "@/public/trainerMyPageIcons/trainerInfo.png";
+import MyInfoChunk from "./components/MyInfoChunk";
+import MyPageButton from "./components/MyPageButton";
 
 export default function TrainerMyPage() {
   return (
     <div className=" bg-white px-2">
       <div className="  text-black px-2">
-        <div className=" flex">
-          <div className="flex">
-            <Image src={userThumb} alt="userThumb"></Image>
-          </div>
-          <div className="flex flex-col w-full">
-            <div className=" flex justify-between items-center">
-              <div className=" text-black text-[20px] font-bold flex items-center">
-                김헬창님
-                <button type="button">
-                  <Image src={infoEditButton} alt="infoEditButton" />
-                </button>
-              </div>
-
-              <button className=" bg-[#22C55E] rounded-[12px] w-[40px] h-[40px] flex justify-center items-center">
-                <Image src={logout} alt="logout" />
-              </button>
-            </div>
-            <div className="text-[17px] text-slate-400">gymgym12@naver.com</div>
-          </div>
-        </div>
-        <div className=" flex my-5">
-          <button className=" flex-1 m-2">
-            <Image src={schedule} alt="schedule" />
-          </button>
-          <button className=" flex-1 m-2">
-            <Image src={programManage} alt="programManage" />
-          </button>
-          <button className=" flex-1 m-2">
-            <Image src={trainerInfo} alt="trainerInfo" />
-          </button>
+        <MyInfoChunk />
+        <div className=" flex my-3">
+          <MyPageButton
+            src={schedule}
+            alt="schedule"
+            content="PT 일정"
+            move="/schedule"
+          />
+          <MyPageButton
+            src={programManage}
+            alt="programManage"
+            content="프로그램 관리"
+            move="/programManage"
+          />
+          <MyPageButton
+            src={trainerInfo}
+            alt="trainerInfo"
+            content="트레이너 정보"
+            move="/introduceTrainer"
+          />
         </div>
       </div>
     </div>
