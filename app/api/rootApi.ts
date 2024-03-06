0d6be69b3455db1cde707594382efe958fb40c04
seongu <tjsrn183@naver.com>
2024-03-07 00:36:32 +0900

    Fix: ssr위해 localStorage에서 쿠키에 토큰을 저장하는방식으로 변경

--- app/api/rootApi.ts
+++ app/api/rootApi.ts
@@ -1,7 +1,14 @@
 import axios from "axios";
-
+import { getCookie } from "cookies-next";
+interface configTypes {
+  method: string;
+  url: string;
+  data?: any;
+  headers?: Record<string, string>;
+  params?: any;
+}
 function getToken() {
-  return localStorage.getItem("token");
+  return getCookie("token");
 }
 
 const apiClient = axios.create({
@@ -13,6 +20,7 @@ apiClient.interceptors.request.use(
   (config) => {
     const token = getToken();
     if (token) {
+      console.log("토큰이 있나유", token);
       config.headers["Authorization"] = `Bearer ${token}`;
     }
     return config;
@@ -22,21 +30,34 @@ apiClient.interceptors.request.use(
   },
 );
 
-export async function sendRequest(endpoint: string, method = "get", data: any) {
+export async function sendRequest(
+  endpoint: string,
+  method: string = "get",
+  data?: any,
+  headers?: Record<string, string>,
+) {
+  const config: configTypes = {
+    method: method,
+    url: endpoint,
+    headers: headers || {},
+    data: data,
+  };
+
+  if (method.toLowerCase() === "get") {
+    delete config["data"];
+  }
   try {
-    const response = await apiClient({
-      method: method,
-      url: endpoint,
-      data: data,
-    });
+    console.log("시작한다아");
+    const response = await apiClient(config);
+    console.log("노오오오옹");
     return response.data;
   } catch (error) {
     console.error("error message:", error);
     throw error;
   }
 }
-
 // 예시 사용법
 // sendRequest('endpoint', 'post', { key: 'value' })
-//   .then(data => console.log(data))
-//   .catch(error => console.error(error));
+//회원가입시
+//sendRequest('join','post',~~);
+// 참고) ssr을 위한 prefetchQuery사용시 trainerMyPage => page.tsx 참고
