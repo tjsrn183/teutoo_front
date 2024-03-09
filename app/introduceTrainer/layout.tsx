a68601852ec723c0e15e21dfe51de21c06f283b3
seongu <tjsrn183@naver.com>
2024-03-09 23:05:46 +0900

    Feat: 트레이너 소개 data prefetch

--- app/introduceTrainer/layout.tsx
+++ app/introduceTrainer/layout.tsx
@@ -1,16 +1,31 @@
-import Header from "@/components/Header";
 import { SmallHeader } from "@/components/SmallHeader";
 import { ReactNode } from "react";
+import { sendRequest } from "../api/rootApi";
+import {
+  HydrationBoundary,
+  QueryClient,
+  dehydrate,
+} from "@tanstack/react-query";
 
-export default function introduceTrainerLayout({
+const getTrainerIntro = async () => {
+  return await sendRequest("trainer/info/me", "get");
+};
+
+export default async function introduceTrainerLayout({
   children,
 }: {
   children: ReactNode;
 }) {
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery({
+    queryKey: ["trainerIntro"],
+    queryFn: getTrainerIntro,
+  });
+  const dehydratedState = dehydrate(queryClient);
   return (
-    <>
+    <HydrationBoundary state={dehydratedState}>
       <SmallHeader title="트레이너 소개" />
       <div>{children}</div>
-    </>
+    </HydrationBoundary>
   );
 }
