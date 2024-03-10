3419c9d1b2a6814f5d5fe11b6f420f4c6bc61efb
seongu <tjsrn183@naver.com>
2024-03-10 14:43:13 +0900

    Feat: zod schema 변경

--- app/introduceTrainer/zodTrainerSchema.ts
+++ app/introduceTrainer/zodTrainerSchema.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 export const zodTrainerSchema = z.object({
-  simpleInfo: z.string().min(10, { message: "소개는 5글자이상 해주세요" }),
-  content: z.string().min(5, { message: "내용을 5글자이상 입력하세요" }),
-  gymName: z.string().min(5, { message: "소개는 5글자이상 해주세요" }),
+  simpleInfo: z.string().min(3, { message: "소개를 더 해주세요" }),
+  content: z.string().min(3, { message: "내용을 더 입력하세요" }),
+  gymName: z.string().min(3, { message: "소개는 더 해주세요" }),
 });
