f19c1d525388fb8344a47feb2baba7c2637319e0
seongu <tjsrn183@naver.com>
2024-03-11 19:56:26 +0900

    Feat: 초기에 렌더링시 데이터 존재 여부에따른 ref 생성

--- app/(afterLogin)/programManage/components/ProgramManageComponent.tsx
+++ app/(afterLogin)/programManage/components/ProgramManageComponent.tsx
@@ -8,7 +8,7 @@ import { useQueryClient } from "@tanstack/react-query";
 interface SubmitFunc {
   submitForm: () => void;
 }
-interface getProgramList {
+export interface getProgramList {
   ptProgramResList: Array<ProgramFormType>;
 }
 export const ProgramManageComponent = () => {
@@ -21,19 +21,33 @@ export const ProgramManageComponent = () => {
   >([]);
 
   const [programForms, setProgramForms] = useState<Array<JSX.Element>>([]);
+  // 사용자가 저장하기 버튼 클릭시 Submit을 트리거하기 위함
   const onSubmitTrigger = () => {
     formRefs.forEach((formRef) => {
       formRef.current?.submitForm();
     });
   };
+  // 사용자가 프로그램 추가 버튼 클릭시 새로운 폼을 추가하기 위함
   const addForm = () => {
     const newRef = React.createRef<SubmitFunc>();
     const newComponent = <ProgramForm key={programForms.length} ref={newRef} />;
     setProgramForms([...programForms, newComponent]);
     setFormRefs([...formRefs, newRef]);
   };
+  // 서버 data가 존재할시 새로운 프로그램 추가 시 새로운 ref 추가하기 위함
+  const addRefsForExistingData = () => {
+    if (data?.ptProgramResList) {
+      const newRefs = data.ptProgramResList.map(() =>
+        React.createRef<SubmitFunc>(),
+      );
+      setFormRefs((prevRefs) => [...prevRefs, ...newRefs]);
+    }
+  };
+
+  // 마운트 시 미리 폼하나를 렌더링, 서버데이터가 존재할 시 ref를 마련해놓음
   useEffect(() => {
     addForm();
+    addRefsForExistingData();
   }, []);
   return (
     <div className=" px-3  bg-white">
@@ -48,9 +62,19 @@ export const ProgramManageComponent = () => {
         </LightButton>
       </div>
       {data?.ptProgramResList.map((item, index) => (
-        <ProgramForm programList={item} key={index} fromServer />
+        <ProgramForm
+          programList={item}
+          key={index}
+          ref={formRefs[index]}
+          fromServer
+        />
       ))}
-      {programForms.map((component) => component)}
+      {programForms.map((component, index) => (
+        <ProgramForm
+          key={index}
+          ref={formRefs[index + (data?.ptProgramResList.length || 0)]}
+        />
+      ))}
       <div className=" flex justify-center sticky bottom-2 right-3 left-3 ">
         <button
           className="m-2 h-[48px] bg-[#1C743C] rounded-[6px] w-full sticky text-white"
