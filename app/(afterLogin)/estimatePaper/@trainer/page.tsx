76e2aca001ee9978a62914545cd24f83cd5daa4a
seongu <tjsrn183@naver.com>
2024-03-12 23:58:27 +0900

    Feat: 트레이너 견적서 작성 api 함수 작성

--- app/(afterLogin)/estimatePaper/@trainer/page.tsx
+++ app/(afterLogin)/estimatePaper/@trainer/page.tsx
@@ -1,11 +1,29 @@
+import {
+  HydrationBoundary,
+  QueryClient,
+  dehydrate,
+} from "@tanstack/react-query";
+
 import { SmallHeader } from "@/components/SmallHeader";
 import EstimateFormTrainer from "../components/EstimateFormTrainer";
-export default function EstimateTrainerPage() {
+import { sendRequest } from "@/app/api/rootApi";
+const fetchOnlyTrainerPrograms = async () => {
+  return await sendRequest("trainer/estimates/programs");
+};
+export default async function EstimateTrainerPage() {
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery({
+    queryKey: ["onlytrainerProgram"],
+    queryFn: fetchOnlyTrainerPrograms,
+  });
+  const dehydratedState = dehydrate(queryClient);
   return (
     <div className=" flex flex-col justify-center h-full">
       <SmallHeader title="견적서 작성" />
       <div className=" w-full h-full bg-gray-200 flex items-center">
-        <EstimateFormTrainer />
+        <HydrationBoundary state={dehydratedState}>
+          <EstimateFormTrainer />
+        </HydrationBoundary>
       </div>
     </div>
   );
