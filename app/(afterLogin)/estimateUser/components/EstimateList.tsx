6cd7ab5800e64c1b3fcd561b7a5e68ae2e0cea65
seongu <tjsrn183@naver.com>
2024-03-14 17:57:55 +0900

    Feat: 기존에 가짜데이터를 서버에서 가져오는 data로 수정

--- app/(afterLogin)/estimateUser/components/EstimateList.tsx
+++ app/(afterLogin)/estimateUser/components/EstimateList.tsx
@@ -1,13 +1,40 @@
-import EstimateUserAtom from "./EstimateUserAtom";
+"use client";
 
+import {
+  EstimateItemAtom,
+  EstimateItemT,
+  fetchInfiniteEstimateU,
+  getTrainerEstimates,
+} from "../layout";
+import EstimateUserAtom from "./EstimateUserAtom";
+import { InfiniteData, useInfiniteQuery } from "@tanstack/react-query";
 export default function EstimateList() {
+  const { data, fetchNextPage, hasNextPage, isFetching } = useInfiniteQuery<
+    getTrainerEstimates,
+    Object,
+    InfiniteData<EstimateItemT>,
+    Array<string>,
+    number
+  >({
+    queryKey: ["trainerEstimates"],
+    queryFn: fetchInfiniteEstimateU,
+    initialPageParam: 0,
+    getNextPageParam: (lastPage) => {
+      if (lastPage?.pageParams && lastPage?.pageParams[0] !== 0) {
+        return lastPage?.pageParams[0] + 1;
+      } else {
+        return 0;
+      }
+    },
+  });
+  console.log("sdfsd", data);
   return (
     <div className=" flex flex-col">
-      <EstimateUserAtom />
-      <EstimateUserAtom />
-      <EstimateUserAtom />
-      <EstimateUserAtom />
-      <EstimateUserAtom />
+      {data?.pages[0].data.map((page: EstimateItemAtom, i) => (
+        <div key={i}>
+          <EstimateUserAtom data={page} />
+        </div>
+      ))}
     </div>
   );
 }
