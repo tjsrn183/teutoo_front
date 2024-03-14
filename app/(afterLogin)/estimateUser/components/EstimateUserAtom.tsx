38666986d437db62f6abb2897a4a52dc38b4613b
seongu <tjsrn183@naver.com>
2024-03-14 17:57:12 +0900

    Feat: 이미지 data존재여부에따라 다른 컴포넌트 렌더링

--- app/(afterLogin)/estimateUser/components/EstimateUserAtom.tsx
+++ app/(afterLogin)/estimateUser/components/EstimateUserAtom.tsx
@@ -3,10 +3,11 @@ import Image from "next/image";
 import userThumb from "@/public/trainerMyPageIcons/userThumb.png";
 import LightButton from "@/components/LightButton";
 import { EstimateModal, ModalDialogHandle } from "./EstimateModal";
-import { MutableRefObject, RefObject, useRef } from "react";
-export default function EstimateUserAtom() {
-  const dialogRef: RefObject<ModalDialogHandle> = useRef(null);
+import { RefObject, useRef } from "react";
+import { EstimateItemAtom } from "../layout";
 
+export default function EstimateUserAtom({ data }: { data: EstimateItemAtom }) {
+  const dialogRef: RefObject<ModalDialogHandle> = useRef(null);
   const handleOpenModal = () => {
     dialogRef.current?.openModal();
   };
@@ -15,11 +16,21 @@ export default function EstimateUserAtom() {
     <div className=" bg-white h-[82px] rounded-[12px]  text-black text-sm my-2 font-bold drop-shadow-lg mx-2">
       <div className=" w-full flex items-center justify-center h-full">
         <div className=" flex justify-center items-center mx-3">
-          <Image src={userThumb} alt="userThumb" width={60} height={60} />
-          <span className="mx-3">김헬창</span>
+          {data.profileImagePath ? (
+            <Image
+              src={data.profileImagePath}
+              alt="userProfile"
+              width={60}
+              height={60}
+            />
+          ) : (
+            <Image src={userThumb} alt="userThumb" width={60} height={60} />
+          )}
+
+          <span className="mx-3">{data.name}</span>
         </div>
 
-        <span className="flex justify-center mx-2">24000 ₩</span>
+        <span className="flex justify-center mx-2">{data.price} ₩</span>
         <LightButton width="w-[25%]" onClick={handleOpenModal}>
           낙찰
         </LightButton>
