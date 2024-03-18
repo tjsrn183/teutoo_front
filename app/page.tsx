8ecb8be1d90b4b6816ad9f56ac718b0a7f42d3f8
전동헌 <56753677+qaws7791@users.noreply.github.com>
2024-03-18 19:57:55 +0900

    Feat: 필터 기능 추가

--- app/page.tsx
+++ app/page.tsx
@@ -9,13 +9,27 @@ import {
   dehydrate,
 } from "@tanstack/react-query";
 import getTrainerList from "@/api/getTrainerList";
+import SearchContainer from "@/app/_components/search-container";
 
-export default async function Home() {
+interface HomeProps {
+  searchParams: {
+    sort: "alpha" | "review";
+    direction: "asc" | "desc";
+  };
+}
+
+export default async function Home({ searchParams }: HomeProps) {
+  const sort = ["alpha", "review"].includes(searchParams.sort)
+    ? searchParams.sort
+    : "alpha";
+  const direction = ["asc", "desc"].includes(searchParams.direction)
+    ? searchParams.direction
+    : "asc";
+
   const queryClient = new QueryClient();
   await queryClient.prefetchInfiniteQuery({
     queryKey: ["trainerList"],
-    queryFn: () =>
-      getTrainerList({ page: 0, size: 5, sort: "alpha", direction: "asc" }),
+    queryFn: () => getTrainerList({ page: 0, size: 5, sort, direction }),
     initialPageParam: 0,
   });
   const dehydratedState = dehydrate(queryClient);
@@ -27,10 +41,7 @@ export default async function Home() {
           <AddressMenu />
         </AppBar>
         <div className="p-2">
-          <Search
-            className="w-full"
-            placeholder="트레이너, 헬스장 이름을 검색하세요..."
-          />
+          <SearchContainer sort={sort} direction={direction} />
           <div className="flex-auto h-full">
             <Suspense fallback={<div>Loading...</div>}>
               <TrainerList />
