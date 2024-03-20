f7aa2d2d6c85932571dcca8a2dbefb9195a8aa0a
seongu <tjsrn183@naver.com>
2024-03-21 00:24:59 +0900

    Design: 요소간 거리가 균등하게 수정

--- app/(afterLogin)/estimateUser/components/EstimateUserAtom.tsx
+++ app/(afterLogin)/estimateUser/components/EstimateUserAtom.tsx
@@ -13,29 +13,32 @@ export default function EstimateUserAtom({ data }: { data: EstimateItemAtom }) {
   };
 
   return (
-    <div className=" bg-white h-[82px] rounded-[12px]  text-black text-sm my-2 font-bold drop-shadow-lg mx-2">
-      <div className=" w-full flex items-center justify-center h-full">
-        <div className=" flex justify-center items-center mx-3">
-          {data.profileImagePath ? (
-            <Image
-              src={data.profileImagePath}
-              alt="userProfile"
-              width={60}
-              height={60}
-            />
-          ) : (
-            <Image src={userThumb} alt="userThumb" width={60} height={60} />
-          )}
+    <div className="bg-white h-[82px] rounded-[12px] text-black text-sm my-2 font-bold drop-shadow-lg mx-2 flex items-center justify-between">
+      <div className="flex items-center ml-3">
+        {data.profileImagePath ? (
+          <Image
+            src={data.profileImagePath}
+            alt="userProfile"
+            width={60}
+            height={60}
+          />
+        ) : (
+          <Image src={userThumb} alt="userThumb" width={60} height={60} />
+        )}
+        <span
+          className="mx-3 whitespace-nowrap overflow-hidden text-ellipsis"
+          style={{ maxWidth: "150px" }}
+        >
+          {data.name}
+        </span>
+      </div>
 
-          <span className="mx-3">{data.name}</span>
-        </div>
-
-        <span className="flex justify-center mx-2">{data.price} ₩</span>
-        <LightButton width="w-[25%]" onClick={handleOpenModal}>
+      <span>{data.price} ₩</span>
+      <div className="mr-3">
+        <LightButton width="w-[70px]" onClick={handleOpenModal}>
           낙찰
         </LightButton>
       </div>
-
       <EstimateModal ref={dialogRef} />
     </div>
   );
