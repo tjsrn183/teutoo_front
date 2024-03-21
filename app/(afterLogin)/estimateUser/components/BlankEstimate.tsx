12c88ba4c38ba40b01db7c53c27743e39cf81115
seongu <tjsrn183@naver.com>
2024-03-21 21:47:41 +0900

    Design: 디자인수정

--- app/(afterLogin)/estimateUser/components/BlankEstimate.tsx
+++ app/(afterLogin)/estimateUser/components/BlankEstimate.tsx
@@ -1,10 +1,10 @@
 export default function BlankEstimate() {
   return (
-    <div className=" w-screen h-[220px]">
-      <div className=" bg-white absolute right-8 left-8 h-[185px] top-5 flex flex-col justify-center items-start rounded-xl px-6 drop-shadow-lg">
-        <div className=" bg-gray-200 w-[100%] h-[50%] rounded-xl mt-5"></div>
-        <div className="w-[80%] h-[20%] bg-gray-200 rounded-xl my-5"></div>
-        <div className="w-[50%] h-[20%] bg-gray-200 rounded-xl mb-5"></div>
+    <div className=" w-full relative flex justify-center h-[220px]  top-5">
+      <div className=" animate-pulse  mx-5 w-full h-[185px] bg-white top-5 flex flex-col justify-center items-start rounded-xl px-6 drop-shadow-lg">
+        <div className=" bg-gray-200 w-[100%]  h-[15%] rounded-xl mt-5"></div>
+        <div className="w-[50%] h-[15%] bg-gray-200 rounded-xl my-5"></div>
+        <div className="w-[70%] h-[15%] bg-gray-200 rounded-xl mb-5"></div>
       </div>
     </div>
   );
