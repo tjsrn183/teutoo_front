09fc966c347c8a07ff5d1a8e4c60ca30390127cd
seongu <tjsrn183@naver.com>
2024-03-23 16:11:49 +0900

    Feat: 유저데이터의 주소정보에 따른 디폴트 지역검색기능

---
+++ app/(afterLogin)/estimateTrainer/components/InputLocation.tsx
@@ -0,0 +1,31 @@
+"use client";
+import SearchInput from "@/components/formElement/SearchInput";
+import { UserDataType } from "../../trainerMyPage/components/MyInfoChunk";
+import { useEffect } from "react";
+import { locationChunk } from "../lib/locationChunk";
+import { useQueryClient } from "@tanstack/react-query";
+import { useUserLocation } from "@/store/useUserLocation";
+
+export default function InputLocation() {
+  const queryClient = useQueryClient();
+  const { location, setLocation } = useUserLocation();
+  const userData: UserDataType | undefined = queryClient.getQueryData([
+    "userData",
+  ]);
+
+  useEffect(() => {
+    if (userData?.data.name) {
+      setLocation(locationChunk(userData?.data.address));
+    }
+  }, []);
+
+  return (
+    <div className=" relative w-[60%] ">
+      <SearchInput
+        placeholder={location || "지역을 입력하세요.."}
+        state={location}
+        setState={setLocation}
+      />
+    </div>
+  );
+}
