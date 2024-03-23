0048beac031ec3b6360a054cd87dd4d295c4db9a
seongu <tjsrn183@naver.com>
2024-03-23 16:05:32 +0900

    Remove: 사용하지않는 이미지,컴포넌트 삭제

--- app/(afterLogin)/estimateTrainer/components/DropdownItems.tsx
+++
@@ -1,7 +0,0 @@
-export default function DropdownItems({ item }: { item: string }) {
-  return (
-    <div className=" text-gray-600  w-[170px] flex justify-center text-base font-bold ">
-      {item}
-    </div>
-  );
-}
