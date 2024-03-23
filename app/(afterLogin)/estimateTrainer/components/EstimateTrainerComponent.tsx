0c46f5d52b63bff54a2bf86cb677f2723f6d827a
seongu <tjsrn183@naver.com>
2024-03-23 16:08:47 +0900

    Refactor: 사용하지않는 코드삭제

--- app/(afterLogin)/estimateTrainer/components/EstimateTrainerComponent.tsx
+++ app/(afterLogin)/estimateTrainer/components/EstimateTrainerComponent.tsx
@@ -1,32 +1,19 @@
 import BlankEstimate from "@/app/(afterLogin)/estimateUser/components/BlankEstimate";
-import DropdownLocation from "./DropdownLocation";
 import EstimateButton from "@/app/(afterLogin)/estimateUser/components/EstimateButton";
 import EstimateTrainerList from "./EstimateTrainerList";
-import Pagenation from "./Pagenation";
+import InputLocation from "./InputLocation";
+import MyEstimateU from "./MyEstimateU";
+
 export default function EstimateTrainerComponent() {
   return (
     <div>
-      <BlankEstimate />
-
-      <div className="flex justify-between mx-2 ">
-        <DropdownLocation />
+      <MyEstimateU />
+      <div className="flex justify-between mx-5 ">
+        <InputLocation />
         <EstimateButton content="견적서" />
       </div>
       <EstimateTrainerList />
-      <div className="pb-[70px]">
-        <Pagenation
-          totalItems={70}
-          currentPage={1}
-          pageCount={5}
-          itemCountPerPage={10}
-        />
-      </div>
+      <div></div>
     </div>
   );
 }
-/*
-  <Pagenation    totalItems={totalBooks}
-          currentPage={page && parseInt(page) > 0 ? parseInt(page) : 1}
-          pageCount={5}
-          itemCountPerPage={50} />
-*/
