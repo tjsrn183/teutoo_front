701221e6f84e7da12b7bc9cf1c6b7f782b596117
seongu <tjsrn183@naver.com>
2024-03-25 13:48:59 +0900

    Chore: 조금의 수정

--- app/(afterLogin)/programManage/page.tsx
+++ app/(afterLogin)/programManage/page.tsx
@@ -1,6 +1,11 @@
+export const metadata: Metadata = {
+  title: "프로그램 관리",
+  description: "프로그램 관리",
+};
 import { ProgramManageComponent } from "./components/ProgramManageComponent";
 import React from "react";
 import TrainerProfile from "./components/TrainerProfile";
+import { Metadata } from "next";
 export default function ProgramManagementPage() {
   return (
     <div className="  bg-white flex flex-col">
