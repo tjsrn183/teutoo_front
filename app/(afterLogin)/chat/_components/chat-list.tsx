61669ea6a4d22a01f733cd91757a6eaf53e4cdfd
qaws7791 <qaws7791@gmail.com>
2024-03-25 19:52:34 +0900

    Feat: ChatList Item Link로 변경

--- app/(afterLogin)/chat/_components/chat-list.tsx
+++ app/(afterLogin)/chat/_components/chat-list.tsx
@@ -4,6 +4,7 @@ import Avatar from "@/components/common/avatar";
 import Badge from "@/components/common/badge";
 import List from "@/components/common/list";
 import Search from "@/components/common/search";
+import Link from "next/link";
 import React from "react";
 
 export default function ChatList(): JSX.Element {
@@ -28,27 +29,33 @@ export default function ChatList(): JSX.Element {
       />
       <List className="mt-4 bg-white">
         {filteredChats.map((chat, i) => (
-          <List.Item className="flex gap-2" key={i}>
-            <Avatar square>
-              <Avatar.Image
-                alt="avatar"
-                src={chat.profileImgUrl || "https://placehold.co/80"}
-              />
-            </Avatar>
-            <div>
-              <p className="font-medium">{chat.name}</p>
-              <p className="text-sm text-neutral-500">
-                {chat.latestChat?.content}
-              </p>
-            </div>
-            <div className="flex-1 text-right text-sm">
-              <p className=" text-neutral-500">오후 3:45</p>
-              {chat.unReadChatCnt > 0 && (
-                <Badge className="mt-1" type="number">
-                  {chat.unReadChatCnt.toString()}
-                </Badge>
-              )}
-            </div>
+          <List.Item key={i} asChild>
+            <Link href={`/chat/${chat.memberId}`} className="flex gap-2">
+              <Avatar square>
+                <Avatar.Image
+                  alt="avatar"
+                  src={chat.profileImgUrl || "https://placehold.co/80"}
+                />
+              </Avatar>
+              <div>
+                <p className="font-medium">{chat.name}</p>
+                <p className="text-sm text-neutral-500">
+                  {chat.latestChat.contentType === "TEXT"
+                    ? chat.latestChat.content.slice(0, 20)
+                    : chat.latestChat.contentType === "RESERVATION"
+                    ? "예약 정보"
+                    : "이미지"}
+                </p>
+              </div>
+              <div className="flex-1 text-right text-sm">
+                <p className=" text-neutral-500">오후 3:45</p>
+                {chat.unReadChatCnt > 0 && (
+                  <Badge className="mt-1" type="number">
+                    {chat.unReadChatCnt.toString()}
+                  </Badge>
+                )}
+              </div>
+            </Link>
           </List.Item>
         ))}
       </List>
