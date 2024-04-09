695401f98dc333305f0d494af9c93a3594c0a1cf
tjsrn183 <tjsrn183@naver.com>
2024-04-09 13:29:19 +0900

    Fix: 사진관련 컴포넌트 오류로 인한 ㅜ정

--- app/(afterLogin)/introduceTrainer/components/PictureExist.tsx
+++ app/(afterLogin)/introduceTrainer/components/PictureExist.tsx
@@ -4,7 +4,6 @@ import Image from "next/image";
 import close from "@/public/join/close.png";
 import { useQueryClient } from "@tanstack/react-query";
 import { useRef, Dispatch, SetStateAction } from "react";
-import { getProgramList } from "../../programManage/components/ProgramManageComponent";
 import { ProgramDataServer } from "../../programManage/components/ProgramForm";
 
 interface ImgProps {
@@ -12,6 +11,7 @@ interface ImgProps {
   setDeleteImg: Dispatch<SetStateAction<Array<string>>>;
   fromWhere?: string;
   programId?: number;
+  programIndex?: number;
 }
 
 export default function PictureExist({
@@ -19,35 +19,30 @@ export default function PictureExist({
   setDeleteImg,
   fromWhere,
   programId,
+  programIndex,
 }: ImgProps) {
   const queryClient = useQueryClient();
+
   const onRemoveImage = (index: number) => () => {
     const updatedImages = fetchImg.filter((_, idx) => idx !== index);
-    console.log("updatedImage", updatedImages);
+
     if (fromWhere === "ProgramManage") {
-      console.log("프로그램 매니지 조건문실행");
       queryClient.setQueryData(
         ["trainerProgram"],
         (prev: { ptProgramResList: ProgramDataServer[] }) => {
-          if (
-            programId !== undefined &&
-            programId >= 0 &&
-            programId < prev.ptProgramResList.length
-          ) {
+          if (programId !== undefined && programId >= 0) {
             const copy = { ...prev };
-            const updatedProgram = { ...copy.ptProgramResList[programId - 1] };
+            const updatedProgram = { ...copy.ptProgramResList[programIndex!] };
+
             updatedProgram.ptProgramImgList = updatedImages;
-            copy.ptProgramResList[programId - 1] = updatedProgram;
-            console.log("카파야ㅕ", copy.ptProgramResList[programId - 1]);
-            // 이 부분에서 변경된 객체를 반환해야 합니다.
-            return copy; // 변경된 객체 반환
+
+            copy.ptProgramResList[programIndex!] = updatedProgram;
+
+            return copy;
           }
-          // programId가 유효하지 않은 경우 기존 상태 반환
-          return prev;
         },
       );
     } else {
-      console.log("인트로 매니지 조건문실행");
       queryClient.setQueryData(
         ["trainerIntro"],
         (prev: { careerImgList: ImgType[] }) => {
